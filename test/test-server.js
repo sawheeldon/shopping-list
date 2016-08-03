fa0f9ad9b26ea9651dd8ad4c8cca2471f2264f94
Simon <simon@jampot.design>
2016-08-03 19:56:09 +0000

    restart

--- test/test-server.js
+++ test/test-server.js
@@ -2,20 +2,27 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server.js');
 
+// chai.should is a CHAI METHOD
 var should = chai.should();
+
+// grabbing objects from server.js
+// via exports.app and etc.
 var app = server.app;
 var storage = server.storage;
 
+// chai is prompted to use http extension
 chai.use(chaiHttp);
 
-describe('Shopping List', function() {
-   it('should list items on GET', function(done) {
+// writes test for the GET function
+describe('Shopping List', function () {
+    it('should list items on GET', function (done) {
         chai.request(app)
             .get('/items')
-            .end(function(err, res) {
+            .end(function (err, res) {
                 should.equal(err, null);
                 res.should.have.status(200);
                 res.should.be.json;
+                // res.body is items array [{}, {}, {}]
                 res.body.should.be.a('array');
                 res.body.should.have.length(3);
                 res.body[0].should.be.a('object');
@@ -29,20 +36,24 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    it('should add an item on POST', function(done) {
+    it('should add an item on post', function (done) {
         chai.request(app)
             .post('/items')
-            .send({'name': 'Kale'})
-            .end(function(err, res) {
+            .send({
+                'name': 'Kale'
+            })
+            .end(function (err, res) {
                 should.equal(err, null);
                 res.should.have.status(201);
                 res.should.be.json;
+                // res.body is the item that was added {name: "", id: #}
                 res.body.should.be.a('object');
                 res.body.should.have.property('name');
                 res.body.should.have.property('id');
                 res.body.name.should.be.a('string');
                 res.body.id.should.be.a('number');
                 res.body.name.should.equal('Kale');
+                res.body.id.should.equal(3);
                 storage.items.should.be.a('array');
                 storage.items.should.have.length(4);
                 storage.items[3].should.be.a('object');
@@ -51,59 +62,63 @@ describe('Shopping List', function() {
                 storage.items[3].id.should.be.a('number');
                 storage.items[3].name.should.be.a('string');
                 storage.items[3].name.should.equal('Kale');
+                storage.items[3].id.should.equal(3);
                 done();
             });
     });
+
     it('should edit an item on put', function (done) {
-    chai.request(app)
-      .put('/items/0')
-      .send({
-        'name': 'cereal'
-      })
-      .end(function (err, res) {
-        should.equal(err, null);
-        res.should.have.status(200);
-        res.should.be.json;
-        // res.body is the edited item {name: "", id: #}
-        res.body.should.be.a('object');
-        res.body.should.have.property('name');
-        res.body.should.have.property('id');
-        res.body.name.should.be.a('string');
-        res.body.id.should.be.a('number');
-        res.body.name.should.equal('cereal');
-        res.body.id.should.equal(0);
-        storage.items[0].should.be.a('object');
-        storage.items[0].should.have.property('name');
-        storage.items[0].should.have.property('id');
-        storage.items[0].name.should.equal('cereal');
-        storage.items[0].id.should.equal(0);
-        storage.items[0].name.should.be.a('string');
-        storage.items[0].id.should.be.a('number');
-        done();
-      });
-  });
-//   it('should delete an item on delete', function(done){
-//         chai.request(app)
-//         .delete('items/1')
-//         .end(function(err, res){
-//             should.equal(err, null);
-//             res.should.have.status(200);
-//             res.should.be.json;
-//         // res.body is the deleted object {name: '', id: #}
-//         res.body.should.be.a('object');
-//         res.body.should.have.property('name');
-//         res.body.should.have.property('id');
-//         res.body.name.should.be.a('string');
-//         res.body.id.should.be.a('number');
-//         res.body.id.should.equal(1);
-//         res.body.name.should.equal('Tomatoes');
-//         storage.items.length.should.equal(3);
-//         storage.items[0].name.should.equal('Broad beans');
-//         storage.items[1].name.should.equal('Tomatoes');
-//         storage.items[2].name.should.equal('Peppers');
-//         done();
-//       });
-  //});
+        chai.request(app)
+            .put('/items/0')
+            .send({
+                'name': 'cereal'
+            })
+            .end(function (err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                // res.body is the edited item {name: "", id: #}
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('cereal');
+                res.body.id.should.equal(0);
+                storage.items[0].should.be.a('object');
+                storage.items[0].should.have.property('name');
+                storage.items[0].should.have.property('id');
+                storage.items[0].name.should.equal('cereal');
+                storage.items[0].id.should.equal(0);
+                storage.items[0].name.should.be.a('string');
+                storage.items[0].id.should.be.a('number');
+                done();
+            });
+    });
+
+
+    it('should delete an item on delete', function (done) {
+        chai.request(app)
+            .delete('items/1')
+            .end(function (err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                // res.body is the deleted object {name: '', id: #}
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.id.should.equal(1);
+                res.body.name.should.equal('Tomatoes');
+                storage.items.length.should.equal(3);
+                storage.items[0].name.should.equal('Broad beans');
+                storage.items[1].name.should.equal('Tomatoes');
+                storage.items[2].name.should.equal('Peppers');
+                done();
+            });
+    });
     it('POST to an ID that exists');
     it('POST without body data');
     it('POST with something other than valid JSON');
